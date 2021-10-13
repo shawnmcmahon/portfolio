33bbaaa52dca0ffda6f81bc379cfda9e9cf589de
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-10-13 16:41:35 -0600

    Begin Routing for main page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,32 @@
 import Header from '../Header/Header';
+import Home from '../Home/Home';
+import Project from '../Project/Project';
+import React, { useEffect, useState } from 'react';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <Header /> 
+      <Switch> 
+        <Route 
+          exact 
+          path='/'
+          render={() => (
+            //Home Component here
+            <Home />
+          )}
+        />
+        <Route 
+          exact 
+          path='/:project/'
+          render={() => (
+            //Home Component here
+            <Project />
+          )}
+        />
+      </Switch>
     </div>
   );
 }
