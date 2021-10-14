b41d192734d4adf90eb9c481aa38cc9b6c459188
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-10-13 18:31:11 -0600

    Add links for Github and LinkedIn

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,6 +7,16 @@ const Home = () => {
         <div className="name-container"> 
           <h1 className="name">Shawn McMahon</h1>
           <h3 className="tagline">Front-End Developer</h3>
+          <a href="https://www.linkedin.com/in/shawnpmcmahon/">
+            <button className="social-button"> 
+              LinkedIn
+            </button>
+          </a>
+          <a href="https://github.com/shawnmcmahon">
+            <button className="social-button"> 
+              Github
+            </button>
+          </a>
         </div>
       </div>
     </div>
