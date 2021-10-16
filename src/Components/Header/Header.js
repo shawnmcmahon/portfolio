9531b1736865600f339d0180ce352916ac7323b9
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-10-16 16:48:03 -0600

    Fix name size and add nav buttons

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,6 +4,9 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav">
+        <button className="nav-button">About</button>
+        <button className="nav-button">Projects</button>
+        <button className="nav-button">Contact</button>
       </nav>
     </header>
   );
