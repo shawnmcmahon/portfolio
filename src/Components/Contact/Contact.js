43e06259543d014688d1903443e5f9a6eb8297fd
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-10-18 16:32:01 -0600

    Push up whatI have so far

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -8,10 +8,23 @@ const Contact = () => {
           CONTACT SHAWN
         </div>
         <div className="contact-info"> 
-          <div className="email-container"> 
-            Email
+          <div className="label-container"> 
+            EMAIL
           </div>
-        
+          <div clasName="contact-container">
+            shawnmcmahon17@gmail.com
+          </div>
+          <div className="label-container"> 
+            SOCIAL MEDIA
+          </div>
+          <div clasName="contact-container">
+          <a href="https://github.com/shawnmcmahon" className="contact">
+              Github
+          </a>
+          <a href="https://www.linkedin.com/in/shawnpmcmahon/" className="contact">
+              LinkedIn
+          </a>
+          </div>
         </div>
       </section>
     </div>
