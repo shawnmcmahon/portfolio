abd1081791e3c546fc850442bbb7bc870814293e
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-10-19 19:31:36 -0600

    Remove visited link color from social media links

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -11,7 +11,7 @@ const Contact = () => {
           <div className="label-container"> 
             EMAIL
           </div>
-          <div clasName="contact-container">
+          <div clasName="contact">
             shawnmcmahon17@gmail.com
           </div>
           <div className="label-container"> 
