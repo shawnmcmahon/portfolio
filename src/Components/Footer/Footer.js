f5a012fa9579aeb90b5466e3e5a316d91b156da9
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-10-20 14:51:27 -0600

    Download all png logos for the footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,7 +6,12 @@ const Footer = () => {
       <div className="contact-info-container"> 
         <div className="contact-label-container"> 
           CONTACT SHAWN
-
+        </div>
+        <div className="contact-value-container"> 
+          shawnmcmahon17@gmail.com
+        </div>
+        <div className="contact-label-container"> 
+          FOLLOW SHAWN
         </div>
       </div>
     </div>
