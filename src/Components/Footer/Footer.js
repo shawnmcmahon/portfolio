b5c7bc8a52e800df86a3d3c7dfba0c4dbcee4246
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-10-20 14:41:57 -0600

    Add contact label container

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,10 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <div className="contact-info-container"> 
-        Hello World
+        <div className="contact-label-container"> 
+          CONTACT SHAWN
+
+        </div>
       </div>
     </div>
   );
