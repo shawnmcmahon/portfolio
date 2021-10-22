a176534b8e0599ce304956e575029392a094bff4
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-10-22 10:57:19 -0600

    Create a filler about me section to implement smooth scroll

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,16 +2,19 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div>
+    <section className="home-container">
       <div className="introduction-container">
-        <section className="section-container"> 
+        <section className="section-container home-name"> 
           <h1 className="name">Shawn McMahon</h1>
           <h3 className="tagline">Front-End Developer</h3>
         </section>
-        <section className="section-container"> 
+        <section className="section-container about"> 
+          About Me  
+
+          This is the About Me Section
         </section>
       </div>
-    </div>
+    </section>
   );
 }
 
