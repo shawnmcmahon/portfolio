f938bc6873332dac94aecfff999f2737dc337380
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-10-25 22:12:01 -0600

    Add portfolio title

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
           <ScrollLink smooth to="/#about" >
            <button className="nav-button">About</button></ScrollLink>
           <Link to="/projects/" > 
-           <button className="nav-button">Projects</button></Link>
+           <button className="nav-button">Portfolio</button></Link>
           <Link to="/contact/">
           <button className="nav-button">Contact</button></Link>
         </div>
