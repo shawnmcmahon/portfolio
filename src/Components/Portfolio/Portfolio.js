8a9ffb32d32e27d875880863987b754ce8975e0e
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-10-26 23:33:47 -0600

    Add Better Jeopardy data to the Portfolio Component

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -5,7 +5,23 @@ import { useState } from 'react';
 
 
 const Portfolio = () => {
-  const [portfolio, setPortfolio] = useState('')
+  const [portfolio, setPortfolio] = useState({
+    projects: {
+      betterJeopardy: {
+        time: "60 Hours / 2 weeks",
+        collaborators: ["Bobby v, Dean C"],
+        technolgoiesUsed: ["React", "Express", "PostgreSQL", "Cypress", "Heroku"], 
+        description: "Better Jeopardy is a trivia game geared towards users who aren't very good at Jeopardy. This nod to the classic game show includes questions from the show. The multiple-choice feature for each question allows users to enjoy the game without needing to be a walking encyclopedia. Users can save their scores and compete for the top spot on the high school board."
+      },
+        learningGoals: ["1. Create a custom API utilizing Express JS", "2. Use PostgreSQL to create a database to store questions and saved games"]
+      // rancidTomatillos: {
+
+      // }, 
+      // whatsCookin: {
+
+      // }
+    }
+  })
 
   return (
     <section className="projects-outter" id="projects">
