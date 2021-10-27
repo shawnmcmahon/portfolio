6acb012c812a4f633cf2f543a1ffdce084970c00
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-10-26 23:37:23 -0600

    Pass down portfolio data into Project component

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -13,7 +13,12 @@ const Portfolio = () => {
         technolgoiesUsed: ["React", "Express", "PostgreSQL", "Cypress", "Heroku"], 
         description: "Better Jeopardy is a trivia game geared towards users who aren't very good at Jeopardy. This nod to the classic game show includes questions from the show. The multiple-choice feature for each question allows users to enjoy the game without needing to be a walking encyclopedia. Users can save their scores and compete for the top spot on the high school board."
       },
-        learningGoals: ["1. Create a custom API utilizing Express JS", "2. Use PostgreSQL to create a database to store questions and saved games"]
+        learningGoals: ["1. Create a custom API utilizing Express JS", "2. Use PostgreSQL to create a database to store questions and saved games"], 
+        pictures: [
+        '../../assets/better-jeopardy/bj1.png',
+        '../../assets/better-jeopardy/bj2.png', 
+        '../../assets/better-jeopardy/bj3.png' 
+      ]
       // rancidTomatillos: {
 
       // }, 
@@ -27,7 +32,7 @@ const Portfolio = () => {
     <section className="projects-outter" id="projects">
       <h2 className="portfolio-title">Projects</h2>
         <div className='portfolio-container'> 
-          <Project />
+          <Project portfolio={portfolio} />
         </div>
     </section>
   );
