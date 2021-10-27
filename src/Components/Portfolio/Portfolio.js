2544e70cfa61050fa8ec4fcb02ff49d0f71e8a2c
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-10-27 00:08:27 -0600

    Begin transfer of individual projects being mapped into Project components

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,38 +1,44 @@
 import './Portfolio.css';
 import Project from '../Project/Project';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 
 const Portfolio = () => {
-  const [portfolio, setPortfolio] = useState({
-    projects: {
-      betterJeopardy: {
+  const [portfolio, setPortfolio] = useState([
+    {
+        number: 1, 
+        title: "Better Jeopardy",
         time: "60 Hours / 2 weeks",
         collaborators: ["Bobby v, Dean C"],
         technolgoiesUsed: ["React", "Express", "PostgreSQL", "Cypress", "Heroku"], 
-        description: "Better Jeopardy is a trivia game geared towards users who aren't very good at Jeopardy. This nod to the classic game show includes questions from the show. The multiple-choice feature for each question allows users to enjoy the game without needing to be a walking encyclopedia. Users can save their scores and compete for the top spot on the high school board."
-      },
+        description: "Better Jeopardy is a trivia game geared towards users who aren't very good at Jeopardy. This nod to the classic game show includes questions from the show. The multiple-choice feature for each question allows users to enjoy the game without needing to be a walking encyclopedia. Users can save their scores and compete for the top spot on the high school board.",
         learningGoals: ["1. Create a custom API utilizing Express JS", "2. Use PostgreSQL to create a database to store questions and saved games"], 
         pictures: [
         '../../assets/better-jeopardy/bj1.png',
         '../../assets/better-jeopardy/bj2.png', 
         '../../assets/better-jeopardy/bj3.png' 
       ]
-      // rancidTomatillos: {
-
-      // }, 
-      // whatsCookin: {
-
-      // }
     }
+])
+
+
+console.log('portfolio', portfolio)
+
+  const projectPreviews = portfolio.map((currentProject, index) => {
+    return (
+      <Project 
+        project={currentProject}
+        key={index}
+      />
+    )
   })
 
   return (
     <section className="projects-outter" id="projects">
       <h2 className="portfolio-title">Projects</h2>
         <div className='portfolio-container'> 
-          <Project portfolio={portfolio} />
+          {!!portfolio && projectPreviews}
         </div>
     </section>
   );
--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,24 +1,29 @@
 import './Project.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import bj1 from '../../assets/better-jeopardy/bj1.png';
 import bj2 from '../../assets/better-jeopardy/bj2.png';
 import bj3 from '../../assets/better-jeopardy/bj3.png';
 
-const Project = () => {
-  const [photoPreview, setPhotoPreview] = useState(bj1);
+const Project = ({project}) => {
+  console.log('project in project', project)
+  const [photoPreview, setPhotoPreview] = useState('');
+
+  // useEffect(() => {
+ 
+  // }, [project])
 
   const changePhoto = (event) => {
     setPhotoPreview(event.target.id)
   }
-  
+
   return (
     <article className="project-outter-container"> 
       <div className="project-description-container"> 
-        <h3 className="project-title">Better Jeopardy</h3>
+        <h3 className="project-title">{project.title}</h3>
           <p className="label">Project Scope Time</p>
-            <p className="value">60 Hours / 2 Weeks</p>
+            <p className="value">{project.time}</p>
           <p className="label">Collaborators</p>
-            <p className="value">Bobby H,  Dean C. </p>
+            <p className="value">{project.collaborators}</p>
           <p className="label">Technologies Used</p>
             <ul className="tech-stack-list">
               <li className="tech">React</li>
@@ -28,7 +33,7 @@ const Project = () => {
               <li className="tech">Heroku</li>
             </ul>
           <p className="label">Description</p>
-            <p className="value">Better Jeopardy is a trivia game geared towards users who aren't very good at Jeopardy. This nod to the classic game show includes questions from the show. The multiple-choice feature for each question allows users to enjoy the game without needing to be a walking encyclopedia. Users can save their scores and compete for the top spot on the high school board.</p>
+            <p className="value">{project.description}</p>
           <ul className="label">Learning Goals
             <li className="value"> 
               1. Create a custom API utilizing Express JS
@@ -43,13 +48,13 @@ const Project = () => {
           <img src={photoPreview} className="preview-photo" />
         </div>
         <div className="project-thumbnail-container">
-            <img src={bj1} className="thumbnail" id={bj1} onClick={changePhoto} />
+            <img src={project.pictures[0]} className="thumbnail" id={project.pictures[0]}  onClick={changePhoto} />
         </div> 
         <div className="project-thumbnail-container">
-            <img src={bj2} className="thumbnail" id={bj2} onClick={changePhoto}  />
+            <img src={project.pictures[1]} className="thumbnail" id={project.pictures[1]}  onClick={changePhoto}  />
         </div> 
         <div className="project-thumbnail-container">
-            <img src={bj3} className="thumbnail" id={bj3} onClick={changePhoto}  />
+            <img src={project.pictures[2]}  className="thumbnail" id={project.pictures[2]}  onClick={changePhoto}  />
         </div>
       </div>
     </article>
