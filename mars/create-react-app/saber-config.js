7197a0c101fb0afaafb1f731b846d1b4c7bd793a
Shawn McMahon <shawnmcmahon17@gmail.com>
2021-10-31 16:04:53 -0600

    Update findStyleAndDesign import statements

---
+++ mars/create-react-app/saber-config.js
@@ -0,0 +1,49 @@
+// Saber config file
+// https://saber.land/docs/saber-config.html
+
+module.exports = {
+  siteConfig: {
+    // Change this to the production URL of your website
+    url: 'https://example.com'
+  },
+
+  // Use the package `saber-theme-portfolio`
+  // More: https://saber.land/docs/themes.html
+  theme: 'portfolio',
+
+  // Configure the theme
+  themeConfig: {
+    style: 'dark',
+    github: 'shawnmcmahon',
+    twitter: 'DOPE_A_MEAN',
+    sponsorLink: '',
+    sponsorTip: 'Support my work',
+    nav: [
+      {
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'About',
+        link: '/about'
+      }
+    ]
+  },
+
+  permalinks: {
+    page: '/:slug',
+    post: '/posts/:slug'
+  },
+
+  plugins: [
+    {
+      resolve: 'saber-plugin-query-posts'
+    },
+    {
+      resolve: 'saber-plugin-feed',
+      options: {
+        atomFeed: true
+      }
+    }
+  ]
+}
