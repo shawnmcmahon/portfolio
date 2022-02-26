2156125d4ea35033b6114f1f0f91b50d8f355441
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-02-26 15:02:34 -0700

    Add dark mode ternary to wrapper of header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -28,51 +28,53 @@ const Header = ({ pageMode, pageDesign, handleModeSwitch, handleDesignSwitch, to
   
 
   return (
-    <header className={findStyleAndDesign(pageMode, pageDesign, 'header')} tabIndex="-1">
-      <nav className={navBar ? 'nav-bar-active' : 'nav-bar'} tabIndex="-1">
-        <div className="logo-container"> 
-          <ScrollLink to="/#home"> 
-            <img src={sm_logo} alt="home button" className="home-button" />
-          </ScrollLink>
-        </div>
-        {window.innerWidth <= 650 && 
-            <label className="switch-wrap switch-html">
-              <input type="checkbox" value={pageMode} onChange={toggleTheme} checked={pageMode === "Dark Mode" ? true : false}/>
-              <div className="switch"></div>
-            </label>
-          }
-        <div className="button-container">
-          {window.innerWidth > 650 && 
-            <label className="switch-wrap switch-html">
-              <input type="checkbox" value={pageMode} onChange={toggleTheme} checked={pageMode === "Dark Mode" ? true : false}/>
-              <div className="switch"></div>
-            </label>
-          }
-          {/* <button className="brush" onClick={handleDesignSwitch}><img src={brush} onClick={handleDesignSwitch}/></button> */}
-          {/* <button class="hamburger" id="hamburger"> 
-            <i class="fas fa-bars"></i>
-          </button>
-            <ul class="nav-ul" id="nav-ul">
-              <ScrollLink smooth to="/#about" ><li>About</li></ScrollLink> 
-              <ScrollLink to="/portfolio/#projects" ><li>Portfolio</li></ScrollLink>
-              <ScrollLink to="/contact/#contact"><li>Contact </li></ScrollLink>
-            </ul> */}
-          {/* {window.innerWidth <= 650 && 
-            <img src={menu} className="menu-button"/>
-          }
-          {window.innerWidth > 650 && */}
-            <> 
-              <ScrollLink smooth to="/#about" >
-              <button className="nav-button">About</button></ScrollLink>
-              <ScrollLink to="/portfolio/#projects" > 
-              <button className="nav-button">Portfolio</button></ScrollLink>
-              <ScrollLink to="/contact/#contact">
-              <button className="nav-button">Contact</button></ScrollLink>
-            </> 
-          {/* } */}
-        </div>
-      </nav>
-    </header>
+    <div className={pageMode ? "Dark Mode" : "Light Mode"}>
+      <header className={findStyleAndDesign(pageMode, pageDesign, 'header')} tabIndex="-1">
+        <nav className={navBar ? 'nav-bar-active' : 'nav-bar'} tabIndex="-1">
+          <div className="logo-container"> 
+            <ScrollLink to="/#home"> 
+              <img src={sm_logo} alt="home button" className="home-button" />
+            </ScrollLink>
+          </div>
+          {window.innerWidth <= 650 && 
+              <label className="switch-wrap switch-html">
+                <input type="checkbox" value={pageMode} onChange={toggleTheme} checked={pageMode === "Dark Mode" ? true : false}/>
+                <div className="switch"></div>
+              </label>
+            }
+          <div className="button-container">
+            {window.innerWidth > 650 && 
+              <label className="switch-wrap switch-html">
+                <input type="checkbox" value={pageMode} onChange={toggleTheme} checked={pageMode === "Dark Mode" ? true : false}/>
+                <div className="switch"></div>
+              </label>
+            }
+            {/* <button className="brush" onClick={handleDesignSwitch}><img src={brush} onClick={handleDesignSwitch}/></button> */}
+            {/* <button class="hamburger" id="hamburger"> 
+              <i class="fas fa-bars"></i>
+            </button>
+              <ul class="nav-ul" id="nav-ul">
+                <ScrollLink smooth to="/#about" ><li>About</li></ScrollLink> 
+                <ScrollLink to="/portfolio/#projects" ><li>Portfolio</li></ScrollLink>
+                <ScrollLink to="/contact/#contact"><li>Contact </li></ScrollLink>
+              </ul> */}
+            {/* {window.innerWidth <= 650 && 
+              <img src={menu} className="menu-button"/>
+            }
+            {window.innerWidth > 650 && */}
+              <> 
+                <ScrollLink smooth to="/#about" >
+                <button className="nav-button">About</button></ScrollLink>
+                <ScrollLink to="/portfolio/#projects" > 
+                <button className="nav-button">Portfolio</button></ScrollLink>
+                <ScrollLink to="/contact/#contact">
+                <button className="nav-button">Contact</button></ScrollLink>
+              </> 
+            {/* } */}
+          </div>
+        </nav>
+      </header>
+    </div>
   );
 }
 
