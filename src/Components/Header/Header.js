35ede5828005c01c7c52cad7d414f15a367840b9
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-06-22 17:00:08 -0600

    Fix issues with Tailwind not loading as a dev dependency

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,77 +1,29 @@
-import '../../styles/desktop/Header.scss';
-import '../../styles/tablet/Header-tablet.scss';
-import '../../styles/phone/Header-phone.scss';
-import { useState } from 'react';
-import { HashLink as ScrollLink } from 'react-router-hash-link';
-import sm_logo from '../../Assets/sm_logo.png';
-import findStyleAndDesign from '../../findStyleAndDesign.js';
+import React, { useState } from 'react';
 
 
-const Header = ({ pageMode, pageDesign, handleModeSwitch, handleDesignSwitch, toggleTheme}) => {
-  const [navBar, setNavBar] = useState(false);
-
-  const changeBackground = () => {
-    if (pageDesign === 'Flat' && window.scrollY >= 50) {
-        setNavBar(true);
-      } else {
-        setNavBar(false);
-      }
-    }
-    
-    if (pageDesign === 'Flat') {
-      window.addEventListener('scroll', changeBackground)
-    }
-
-
-  
-
+const Header = () => {
+  let links = [
+    {name:"HOME",link:"/"},
+    {name:"SERVICE",link:"/"},
+    {name:"ABOUT",link:"/"},
+    {name:"BLOG'S",link:"/"},
+    {name:"CONTACT",link:"/"},
+  ];
+  let [open,setOpen] = useState(false);
   return (
-    <header className={findStyleAndDesign(pageMode, pageDesign, 'header')} tabIndex="-1">
-      <nav className={navBar ? 'nav-bar-active' : 'nav-bar'} tabIndex="-1">
-        <div className="logo-container"> 
-          <ScrollLink to="/#home"> 
-            <img src={sm_logo} alt="home button" className="home-button" />
-          </ScrollLink>
-        </div>
-        {window.innerWidth <= 650 && 
-            <label className="switch-wrap switch-html">
-              <input type="checkbox" value={pageMode} onChange={toggleTheme} checked={pageMode === "Dark Mode" ? true : false}/>
-              <div className="switch"></div>
-            </label>
-          }
-        <div className="button-container">
-          {window.innerWidth > 650 && 
-            <label className="switch-wrap switch-html">
-              <input type="checkbox" value={pageMode} onChange={toggleTheme} checked={pageMode === "Dark Mode" ? true : false}/>
-              <div className="switch"></div>
-            </label>
-          }
-          {/* <button className="brush" onClick={handleDesignSwitch}><img src={brush} onClick={handleDesignSwitch}/></button> */}
-          {/* <button class="hamburger" id="hamburger"> 
-            <i class="fas fa-bars"></i>
-          </button>
-            <ul class="nav-ul" id="nav-ul">
-              <ScrollLink smooth to="/#about" ><li>About</li></ScrollLink> 
-              <ScrollLink to="/portfolio/#projects" ><li>Portfolio</li></ScrollLink>
-              <ScrollLink to="/contact/#contact"><li>Contact </li></ScrollLink>
-            </ul> */}
-          {/* {window.innerWidth <= 650 && 
-            <img src={menu} className="menu-button"/>
-          }
-          {window.innerWidth > 650 && */}
-            <> 
-              <ScrollLink smooth to="/#about" >
-              <button className="nav-button">About</button></ScrollLink>
-              <ScrollLink to="/portfolio/#projects" > 
-              <button className="nav-button">Portfolio</button></ScrollLink>
-              <ScrollLink to="/contact/#contact">
-              <button className="nav-button">Contact</button></ScrollLink>
-            </> 
-          {/* } */}
-        </div>
-      </nav>
-    </header>
-  );
+    <div className='shadow-md w-full fixed top-0 left-0'>
+      <div className='md:flex items-center justify-between bg-black py-4 md:px-10 px-7'>
+      <div className='font-bold text-xl cursor-pointer flex items-center font-[Poppins] 
+      text-gray-800'>
+        <span className='text-3xl text-indigo-600 mr-1 pt-2'>
+        <ion-icon name="logo-ionic"></ion-icon>
+        </span>
+        Designer
+      </div>
+    </div>
+    </div>
+  )
 }
 
-export default Header;
+
+export default Header
